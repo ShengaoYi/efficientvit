1215f40e7ddfe7f277714d3902a3ae7fbb2c8303
shengaoyi <673392438@qq.com>
2023-11-15 12:45:29 -0500

    Update seg predict outputs

--- predict_seg_model.py
+++ predict_seg_model.py
@@ -1,5 +1,8 @@
 import argparse
 import torch
+import os
+import glob
+import pandas as pd
 import numpy as np
 from PIL import Image
 from torchvision import transforms
@@ -20,20 +23,58 @@ def predict(image_path, model, device):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_path", type=str, required=True, help="Path to the Cityscapes image")
+    default_folder_path = '/Users/ericyi/Library/CloudStorage/OneDrive-TempleUniversity/efficientvit/test_dir'
+    default_weight_path = 'assets/checkpoints/seg/cityscapes/l2.pt'
+    default_pid_file = 'San Francisco_2017.csv'
+
+    parser.add_argument("--folder_path", type=str, default=default_folder_path,
+                        help="Path to the folder containing the test_dir")
     parser.add_argument("--model", type=str, default="l2", help="Model name")
-    parser.add_argument("--weight_path", type=str, required=True, help="Path to the model weight file")
+    parser.add_argument("--weight_path", type=str, default=default_weight_path, help="Path to the model weight file")
+    parser.add_argument("--pid_file", type=str, default=default_pid_file, help="Path to the PID file")
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = create_seg_model(args.model, "cityscapes", weight_url=None)
-    model.load_state_dict(torch.load(args.weight_path, map_location=device))
+    model.load_state_dict(torch.load(args.weight_path, map_location=device)['state_dict'])
     model = model.to(device).eval()
 
-    segmentation = predict(args.image_path, model, device)
-    # Do something with the segmentation result, e.g., save it or display it
+    result_file = 'segmentation_results.csv'
+    result_id_file = 'segmentation_results_id.csv'
 
-    print(segmentation)
+    class_labels = ['road', 'sidewalk', 'building', 'wall', 'fence', 'pole', 'traffic light',
+                    'traffic sign', 'vegetation', 'terrain', 'sky', 'person', 'rider', 'car',
+                    'truck', 'bus', 'train', 'motorcycle', 'bicycle']
+
+    if not os.path.exists(result_file):
+        with open(result_file, 'w', encoding='utf-8') as f:
+            header = 'img_id,' + ','.join(class_labels) + '\n'
+            f.write(header)
+
+    n = 0
+
+    # Process each image in the folder
+    for image_path in glob.glob(os.path.join(args.folder_path, '*.jpg')):
+        n += 1
+        segmentation = predict(image_path, model, device)
+        image_name = os.path.basename(image_path).split('.')[0]
+
+        total_pixels = segmentation.size
+        class_counts = np.bincount(segmentation.ravel(), minlength=19)
+        class_proportions = class_counts / total_pixels
+
+        with open(result_file, 'a', encoding='utf-8') as f:
+            line = f'{image_name},' + ','.join(map(str, class_proportions)) + '\n'
+            f.write(line)
+
+        print(f'---------Image {image_name} the {n}th segmentation calculation completed--------')
+
+    # Read PID file and segmentation results, then merge
+    pid_data = pd.read_csv(args.pid_file)
+    segmentation_results = pd.read_csv(result_file)
+    merged_data = pd.merge(pid_data, segmentation_results, left_on='pid', right_on='img_id')
+    merged_data.drop(columns=['img_id']).to_csv(result_id_file, index=False)
+
 
 if __name__ == "__main__":
     main()
